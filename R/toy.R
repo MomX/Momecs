eff7add8152bceb5c94fb3b188f184c07ed75580
vbonhomme <bonhomme.vincent@gmail.com>
2018-03-30 11:23:35 +0200

    cosmetics

--- R/toy.R
+++ R/toy.R
@@ -1,23 +1,3 @@
-# Toy is obtained with the code below
-# library(Momocs)
-#
-# toy <- list(
-#   bot       = bot %>% efourier(5),
-#   chaff     = chaff %>% fgProcrustes(),
-#   charring  = charring %>% coo_bookstein %>% coo_slide(ldk=1) %>% chop(~view) %>% efourier(8, norm=FALSE) %>% combine,
-#   flower    = Momocs::flower,
-#   hearts    = hearts %>% fgProcrustes() %>% coo_slide(ldk=2) %>% efourier(6, norm=FALSE),
-#   molars    = molars %>% fgProcrustes() %>% coo_slide(ldk=1) %>% efourier(8, norm=FALSE),
-#   mosquito  = mosquito %>% coo_slidedirection("right") %>% efourier(6, norm=FALSE),
-#   oak       = oak %>% fgProcrustes(),
-#   olea      = olea %>% filter(var != "Cypre") %>% chop(~view) %>% opoly(6) %>% combine,
-#   shapes    = shapes %>% efourier(5),
-#   trilo     = trilo %>% efourier(5),
-#   wings     = wings %>% fgProcrustes()
-# )
-# class(toy) %<>% c(., "toy")
-# devtools::use_data(toy, overwrite = TRUE)
-
 #' Toy dataset from Momocs
 #'
 #' @docType data
@@ -41,4 +21,26 @@
 #' @source [Momocs]
 #' @family datasets
 NULL
+
+
+
+# Toy is obtained with the code below
+# library(Momocs)
+#
+# toy <- list(
+#   bot       = bot %>% efourier(5),
+#   chaff     = chaff %>% fgProcrustes(),
+#   charring  = charring %>% coo_bookstein %>% coo_slide(ldk=1) %>% chop(~view) %>% efourier(8, norm=FALSE) %>% combine,
+#   flower    = Momocs::flower,
+#   hearts    = hearts %>% fgProcrustes() %>% coo_slide(ldk=2) %>% efourier(6, norm=FALSE),
+#   molars    = molars %>% fgProcrustes() %>% coo_slide(ldk=1) %>% efourier(8, norm=FALSE),
+#   mosquito  = mosquito %>% coo_slidedirection("right") %>% efourier(6, norm=FALSE),
+#   oak       = oak %>% fgProcrustes(),
+#   olea      = olea %>% filter(var != "Cypre") %>% chop(~view) %>% opoly(6) %>% combine,
+#   shapes    = shapes %>% efourier(5),
+#   trilo     = trilo %>% efourier(5),
+#   wings     = wings %>% fgProcrustes()
+# )
+# class(toy) %<>% c(., "toy")
+# devtools::use_data(toy, overwrite = TRUE)
 
